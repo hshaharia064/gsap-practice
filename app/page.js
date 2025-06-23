af023d7144324d5f246ece62e8ecd1856753c9ac
Shaharia hasan <hshaharia064@gmail.com>
2025-06-23 15:19:17 +0530

    learned svg animation in gsap

--- app/page.js
+++ app/page.js
@@ -2,7 +2,9 @@
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
-import ScrollTrigger from "gsap-trial/ScrollTrigger";
+import { ScrollTrigger } from "gsap/all";
+// import ScrollTrigger from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger)
 
 export default function Home() {
   // const navbox = useRef(null)
@@ -103,6 +105,89 @@ export default function Home() {
 
 // },[])
 
+// let initalPath = "M 10 80 Q 500 80 1000 80";
+// let finalpath = "M 10 80 Q 500 80 1000 80"
+
+const divRef = useRef(null)
+
+useEffect(()=>{
+
+  gsap.from('#page1 #box1',{
+    rotate : 360,
+    duration : 3
+  })
+  gsap.from('#page2 #box2',{
+    rotate : 360,
+    duration : 3,
+    scrollTrigger : { 
+     trigger :  "#page2 #box2",
+     start : "top 80%",
+     end : 'top 50%',
+     scrub : 2,
+    //  markers : 'true'
+    }
+  })
+
+
+  gsap.from('#page3 #box3',{
+    x : 1000,
+    rotateY : 360,
+    yoyo : 'true',
+    duration : 2,
+    
+    scrollTrigger : {
+      trigger : '#page3 #box3',
+      start : 'top 80%',
+      end : 'bottom 40%',
+      // markers : 'true'
+    }
+  })
+
+
+gsap.to('#vScroll h1',{
+  transform : 'translateX(-80%)',
+  scrollTrigger : {
+    trigger : '#vScroll',
+    // markers : 'true',
+    start : 'top 0%',
+    end : 'top -100%',
+    scrub : 5,
+    pin : true
+  }
+})
+
+},[])
+
+
+
+const mouseEnter = (e)=>{
+  const boundingBox = e.currentTarget.getBoundingClientRect();
+const relativeY = e.clientY - boundingBox.top;
+
+   let path = `M 10 200 Q ${e.clientX} ${relativeY} 1600 200`
+  console.log(path);
+  
+gsap.to('svg path',{
+  attr : {
+    d : path
+  },
+  ease : 'elastic.out(1,0.2)',
+})
+  
+}
+
+const mouseLeave = (details)=>{
+  console.log(details)
+  let path = `M 10 200 Q 800 200 1600 200`
+  gsap.to('svg path',{
+    attr : {
+      d : path
+    }
+  })
+}
+
+useEffect(()=>{
+},[])
 
 
 
@@ -125,15 +210,46 @@ export default function Home() {
      */}
 
 
-     <div  id="page1" className="h-[100vh] flex items-center justify-center  w-full bg-amber-600">
-      <div className="size-40 bg-cyan-600"></div>
+     <div  id="page1" className="h-[100vh] flex items-center justify-center  w-full bg-red-950">
+      <div id="box1" className="size-40 bg-cyan-600"></div>
      </div>
      <div id="page2" className="h-[100vh]  flex items-center justify-center w-full bg-amber-800">
-      <div className="size-40 bg-cyan-600"></div>
+      <div id="box2" className="size-40 bg-cyan-600"></div>
      </div>
-     <div id="page3" className="h-[100vh]  flex items-center justify-center w-full bg-amber-950">
-      <div className="size-40 bg-cyan-600"></div>
+     <div id="page3" className="h-[100vh] overflow-hidden flex items-center justify-center w-full bg-amber-950">
+      <div id="box3" className="size-40 bg-cyan-600"></div>
      </div>
+
+
+     <div id="vScroll" className="h-[100vh] w-full bg-gray-950 flex items-center px-10 " >
+          <h1  className="text-[50vh] text-gray-300 whitespace-nowrap font-bold uppercase">Hello World, i'm Shaharia Hasan</h1>
+     </div>
+
+
+
+
+
+
+
+
+
+
+     <div ref={divRef} className="h-[40vh] w-full  "
+     onMouseMove={mouseEnter}
+     onMouseLeave={mouseLeave}>
+      <svg width="1600" height="500" >
+  <path
+  
+    d="M 10 200 Q 800 200 1600 200"
+    stroke="white"
+    fill="transparent"/>
+</svg>
+     </div>
+
+
+
+
+     {/* <div className="bg-red-950 w-full h-[100vh]"></div> */}
     </>
   );
 }
